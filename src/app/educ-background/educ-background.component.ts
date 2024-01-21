05c9dc5bd17c110299ac79c80fbde6a79ab2156e
Shayyanne Marasigan <shayyannedominiq.marasigan@letran.edu.ph>
2024-01-21 17:06:25 +0800

    Final Changes in UI

--- src/app/educ-background/educ-background.component.ts
+++ src/app/educ-background/educ-background.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-educ-background',
   templateUrl: './educ-background.component.html',
-  styleUrl: '../../../node_modules/bootstrap/dist/css/bootstrap.min.css'
+  styleUrl: '../../../node_modules/bootstrap/dist/css/bootstrap.min.css',  
 })
 export class EducBackgroundComponent {
 
